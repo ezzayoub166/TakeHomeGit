780d386bef0fd6053e1c26ac37637a66f2822df5
ezzayoub166 <ezzdineayoub@gmail.com>
2024-12-28 21:13:50 +0200

    make tab bar and searchVC and favoritesVC

---
+++ TakeHomeProject/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  TakeHomeProject
+//
+//  Created by ezz on 28/12/2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
---
+++ TakeHomeProject/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  TakeHomeProject
+//
+//  Created by ezz on 28/12/2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
