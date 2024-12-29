9d06e7ae2f7fb0c9a1d99a8afbcac1461513519b
ezzayoub166 <ezzdineayoub@gmail.com>
2024-12-29 09:52:22 +0200

    Create Search Screen progrmaticUI in Advance way

--- TakeHomeProject/SearchVC.swift
+++ TakeHomeProject/SearchVC.swift
@@ -8,11 +8,54 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView       = UIImageView()
+    let usernameTextFiled   = GFTextFiled()
+    let callToActionButton  = GFButton(backgroundColor: .systemGreen, title: "Get Followers ")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextFiled()
+        configureButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView(){
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image                                     = UIImage(named: "gh-logo")
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextFiled(){
+        view.addSubview(usernameTextFiled)
+        NSLayoutConstraint.activate([
+            usernameTextFiled.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextFiled.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextFiled.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextFiled.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureButton(){
+        view.addSubview(callToActionButton)
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
 }
