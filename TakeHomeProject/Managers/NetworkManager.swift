b12f72c220e3131d501b8a1192dafb61788255b3
ezzayoub166 <ezzdineayoub@gmail.com>
2025-01-06 23:03:05 +0200

    Download & Cache Avatar Images

--- TakeHomeProject/Managers/NetworkManager.swift
+++ TakeHomeProject/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by ezz on 05/01/2025.
 //
 
-import Foundation
+import UIKit
 class NetworkManager {
-    static let shared = NetworkManager()
     
-    let baseURL = "https://api.github.com/users/"
+    static let shared = NetworkManager()
+    private let baseURL = "https://api.github.com/users/"
+    let cache   = NSCache<NSString,UIImage>()
     
     private init() {}
     
